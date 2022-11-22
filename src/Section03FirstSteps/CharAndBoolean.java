4d0cc06dc8513e4615e7a9f21b621845044cae80
philippabele <54260843+philippabele@users.noreply.github.com>
2022-11-22 09:54:17 +0000

    boolean example

--- src/Section03FirstSteps/CharAndBoolean.java
+++ src/Section03FirstSteps/CharAndBoolean.java
@@ -5,8 +5,13 @@ public class CharAndBoolean {
 
         char myChar = 'D';
         char myUnicode = '\u0044';
+        // see https://unicode-table.com/en/ for more
         System.out.println(myChar);
         System.out.println(myUnicode);
+
+        boolean myTrueValue = true;
+        boolean myFalseValue = false;
+        System.out.println(myTrueValue + " or " + myFalseValue);
 
     }
 }
