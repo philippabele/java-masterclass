bcfb3b662caada4d5a29989ec629a4ae7f759305
philippabele <54260843+philippabele@users.noreply.github.com>
2022-11-22 09:50:27 +0000

    unicode example

--- src/Section03FirstSteps/CharAndBoolean.java
+++ src/Section03FirstSteps/CharAndBoolean.java
@@ -3,5 +3,10 @@ package Section03FirstSteps;
 public class CharAndBoolean {
     public static void main(String[] args) {
 
+        char myChar = 'D';
+        char myUnicode = '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUnicode);
+
     }
 }
