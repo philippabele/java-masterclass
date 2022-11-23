80c43f8ab6bef68b365bb92099aeddfa0d5ee5b6
philippabele <54260843+philippabele@users.noreply.github.com>
2022-11-23 09:39:10 +0000

    init operator

---
+++ src/Section03FirstSteps/Operator.java
@@ -0,0 +1,10 @@
+package Section03FirstSteps;
+
+public class Operator {
+    public static void main(String[] args) {
+
+        int result = 1 + 2; // my Comment
+        System.out.println(result);
+
+    }
+}
