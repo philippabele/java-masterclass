9b1cefe6d4fb0c81e4880c8845ba4cf65021e96f
philippabele <54260843+philippabele@users.noreply.github.com>
2022-11-23 10:11:32 +0000

    if statement and && operator

--- src/Section03FirstSteps/Operator.java
+++ src/Section03FirstSteps/Operator.java
@@ -19,5 +19,18 @@ public class Operator {
         result *= 10;
         result /= 2;
 
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of them :)");
+        }
+
+        int topScore = 80;
+        int secondTopScore = 60;
+
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Still space for improvement");
+        }
+
     }
 }
