f090810dd46ff350a105e52793a4677c9b9f7206
philippabele <54260843+philippabele@users.noreply.github.com>
2022-11-25 15:20:24 +0000

    methode challenge

--- src/Section05/MethodExample.java
+++ src/Section05/MethodExample.java
@@ -18,6 +18,10 @@ public class MethodExample {
         // you can reuse the return of a methode directly
         System.out.println("Your final score was " +
                 calculateScore(gameOver, score, levelCompleted, bonus));
+
+        System.out.println("Your Position is " +
+                calculateHighScorePosition(newScore));
+
     }
 
     // method with return value ? >> call it a function
@@ -34,6 +38,23 @@ public class MethodExample {
 
         }
         return finalScore;
+
+    }
+
+    // challenge
+    private static int calculateHighScorePosition(int playerScore) {
+
+        int position = 4;
+
+        if (playerScore >= 1000) {
+            position = 1;
+        } else if (playerScore >= 500) {
+            position = 2;
+        } else if (playerScore >= 100) {
+            position = 3;
+        }
+
+        return position;
 
     }
 
