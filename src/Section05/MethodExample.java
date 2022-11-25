a1bf700541b288b85fa38014431f1eee977cc225
philippabele <54260843+philippabele@users.noreply.github.com>
2022-11-25 14:35:31 +0000

    basic methode

--- src/Section05/MethodExample.java
+++ src/Section05/MethodExample.java
@@ -3,5 +3,32 @@ package Section05;
 public class MethodExample {
     public static void main(String[] args) {
 
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+
+        calculateScore();
+
     }
+
+    private static void calculateScore() {
+
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score was " + finalScore);
+        }
+
+    }
+
 }
