69482da0574c98bf32e7a342f2d84debd49c4103
philippabele <54260843+philippabele@users.noreply.github.com>
2022-12-27 10:33:47 +0000

    add ex 37

---
+++ src/Section07OopInheritance/Exercise37PoolArea/Rectangle.java
@@ -0,0 +1,25 @@
+package Section07OopInheritance.Exercise37PoolArea;
+
+public class Rectangle {
+
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        this.width = (width < 0) ? 0 : width;
+        this.length = (length < 0) ? 0 : length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+
+}
